c4c6f67b099b793e14b43a6b58f83ec063b1936b
stagef <rrthrthhrt@yandex.ru>
2022-04-09 23:06:46 +1000

    create vue project & connect bootstrap

---
+++ README.md
@@ -0,0 +1,1 @@
+Project on Django Rest, Vue js, Postgresql
---
+++ vue-rentcar/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+  @import "assets/css/bootstrap/bootstrap.css";
+</style>
