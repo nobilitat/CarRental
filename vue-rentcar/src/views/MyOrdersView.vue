a21460d14632274b2fef5eeb950f90e0488cc369
stagef <rrthrthhrt@yandex.ru>
2022-05-23 01:34:30 +1000

    added output orders

--- vue-rentcar/src/views/MyOrdersView.vue
+++ vue-rentcar/src/views/MyOrdersView.vue
@@ -2,40 +2,68 @@
   <div class="container mt-2 mb-2 px-0">
     <NavAuthUser />
     <h2 class="mt-3 text-center">Мои заказы</h2>
-    <div class="row mt-3 justify-content-center">
-      <div class="col-lg-5 border border-secondary rounded-end pt-2">
-        <p>Статус: state</p>
-        <p>Заказ от date</p>
-      </div>
-    </div>
-    <div class="row mt-3 justify-content-center">
-      <div class="col-lg-5 border border-secondary rounded-end pt-2">
-        <p>Статус: state</p>
-        <p>Заказ от date</p>
-      </div>
-    </div>
-    <div class="row mt-3 justify-content-center">
-      <div class="col-lg-5 border border-secondary rounded-end pt-2">
-        <p>Статус: state</p>
-        <p>Заказ от date</p>
-      </div>
-    </div>
-    <div class="row mt-3 justify-content-center">
-      <div class="col-lg-5 border border-secondary rounded-end pt-2">
-        <p>Статус: state</p>
-        <p>Заказ от date</p>
+    <div v-for="order in orderList" :key="order.id">
+      <div
+        @click="goToOrder(order)"
+        class="row justify-content-center text-center"
+      >
+        <p class="col-lg-6 order-section bg-lightblue border border-secondary rounded-end p-2 mt-2">
+          <span v-if="order.active_state">Статус: Открыт</span>
+          <span v-else>Статус: Закрыт</span> <br />
+          <br />
+          <span>Заказ № {{ order.id }}</span> <br />
+          <span>Дата: {{ order.date_getting }}</span> <br />
+          <span>Сумма заказа: {{ order.order_price }} руб</span> <br />
+        </p>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.order-section:hover {
+  cursor:pointer;
+}
+</style>
 
 <script>
 import NavAuthUser from "../components/NavAuthUser.vue";
 
 export default {
   name: "MyOrdersView",
+  data() {
+    return {
+      orderList: [],
+    };
+  },
   components: {
     NavAuthUser,
+  },
+  created() {
+    this.getOrders();
+  },
+  methods: {
+    async getOrders() {
+      const response = await fetch(
+        `${this.$store.getters.getServerUrl}/cars/order/list`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        }
+      );
+      this.orderList = await response.json();
+    },
+    goToOrder(order) {
+      this.$router.push({
+        path: `/order/${order.id}`,
+        params: {
+          order: order,
+        },
+      });
+    },
   },
 };
 </script>
--- vue-rentcar/src/views/OrderPageView.vue
+++ vue-rentcar/src/views/OrderPageView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="container mt-2 mb-2 px-0">
     <NavAuthUser />
-    <h2 class="mt-3">Заказ {{ number }}</h2>
-
+    <h2 class="mt-3 text-center">Заказ № {{ orderDetails.id }}</h2>
+    <div class="row justify-content-center">
+      <div class="col-lg-5">
+        <p v-if="orderDetails.active_state">СТАТУС: ОТКРЫТ</p>
+        <p v-else>СТАТУС: ЗАКРЫТ</p>
+        <p>Пользователь: {{ orderDetails.customer.user.username }}</p>
+        <p>Автомобиль: {{ orderDetails.car.title }}</p>
+        <p>Дата получения: {{ orderDetails.date_getting }}</p>
+        <p>
+          Адрес доставки:
+          {{ orderDetails.delivery.delivery_zone.area_name }} район,
+          {{ orderDetails.delivery.address }}
+        </p>
+        <p>Время доставки:  {{ orderDetails.delivery.delivery_time }}</p>
+        <p>Срок аренды: {{ orderDetails.period }} дн</p>
+        <p>Стоимость: {{ orderDetails.order_price }} руб</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavAuthUser from "../components/NavAuthUser.vue";
+import {numberFormatter, dateTimeFormatter, dateFormatter} from "../assets/js/additional-functions.js"
 
 export default {
   name: "OrderPageView",
   components: {
     NavAuthUser,
+  },
+  data() {
+    return {
+      orderDetails: {},
+    };
+  },
+  // Не получилось вытащить order (undefined)
+  // props: {
+  //   order: Object
+  // },
+  created() {
+    this.getDataAboutOrder();
+  },
+  methods: {
+    async getDataAboutOrder() {
+      const idOrder = this.$route.params.id;
+      const resposne = await fetch(
+        `${this.$store.getters.getServerUrl}/cars/order/detail/${idOrder}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        }
+      );
+
+      this.orderDetails = await resposne.json();
+      this.orderDetails.order_price = numberFormatter(this.orderDetails.order_price);
+      this.orderDetails.date_getting = dateFormatter(this.orderDetails.date_getting);
+      this.orderDetails.delivery.delivery_time = dateTimeFormatter(this.orderDetails.delivery.delivery_time)
+      
+    },
   },
 };
 </script>
